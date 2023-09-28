6aea57b5dc9ed9dcfc9c663725374c586b15692a
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-09-28 21:27:18 +0700

    Place .babelrc in root folder

--- react-app/src/components/LoginForm/LoginForm.test.tsx
+++ react-app/src/components/LoginForm/LoginForm.test.tsx
@@ -1,10 +1,15 @@
 // import React from "react";
 import { render } from "@testing-library/react";
 import LoginForm from ".";
+import { MemoryRouter } from "react-router-dom";
 
 describe("test login form", () => {
   const mockProps = () => {};
   it("title render correctly", async () => {
-    render(<LoginForm onSubmit={mockProps} />);
+    render(
+      <MemoryRouter>
+        <LoginForm onSubmit={mockProps} />
+      </MemoryRouter>
+    );
   });
 });

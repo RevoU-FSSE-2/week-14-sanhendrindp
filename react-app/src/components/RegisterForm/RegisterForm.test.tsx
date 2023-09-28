a1cd999437fa9e8e2e42f58d82556dafd983f17c
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-09-29 04:49:54 +0700

    test: :test_tube: Test for register form is success
    Test title, label, button, and onSubmit is success

--- react-app/src/components/RegisterForm/RegisterForm.test.tsx
+++ react-app/src/components/RegisterForm/RegisterForm.test.tsx
@@ -1,23 +1,33 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import LoginForm from ".";
 import { MemoryRouter } from "react-router-dom";
+import RegisterForm from ".";
 
-describe("Test login form", () => {
+describe("Test register form", () => {
   const mockProps = jest.fn();
   it("Title render correctly", async () => {
     render(
       <MemoryRouter>
-        <LoginForm onSubmit={mockProps} />
+        <RegisterForm onSubmit={mockProps} />
       </MemoryRouter>
     );
-    const title = screen.getByText("Login Page");
+    const title = screen.getByText("Register Page");
+    expect(title).toBeDefined();
+  });
+
+  it("Label Name render correctly", async () => {
+    render(
+      <MemoryRouter>
+        <RegisterForm onSubmit={mockProps} />
+      </MemoryRouter>
+    );
+    const title = screen.getByText("Name");
     expect(title).toBeDefined();
   });
 
   it("Label Email render correctly", async () => {
     render(
       <MemoryRouter>
-        <LoginForm onSubmit={mockProps} />
+        <RegisterForm onSubmit={mockProps} />
       </MemoryRouter>
     );
     const title = screen.getByText("Email");
@@ -27,52 +37,56 @@ describe("Test login form", () => {
   it("Label Password render correctly", async () => {
     render(
       <MemoryRouter>
-        <LoginForm onSubmit={mockProps} />
+        <RegisterForm onSubmit={mockProps} />
       </MemoryRouter>
     );
     const title = screen.getByText("Password");
     expect(title).toBeDefined();
   });
 
-  it("Button Login render correctly", async () => {
-    render(
-      <MemoryRouter>
-        <LoginForm onSubmit={mockProps} />
-      </MemoryRouter>
-    );
-    const title = screen.getByText("Login");
-    expect(title).toBeDefined();
-  });
-
   it("Button Register render correctly", async () => {
     render(
       <MemoryRouter>
-        <LoginForm onSubmit={mockProps} />
+        <RegisterForm onSubmit={mockProps} />
       </MemoryRouter>
     );
     const title = screen.getByText("Register");
     expect(title).toBeDefined();
   });
 
-  it("onSubmit for login page works correctly", async () => {
+  it("Button Back render correctly", async () => {
     render(
       <MemoryRouter>
-        <LoginForm onSubmit={mockProps} />
+        <RegisterForm onSubmit={mockProps} />
       </MemoryRouter>
     );
+    const title = screen.getByText("Back");
+    expect(title).toBeDefined();
+  });
+
+  it("onSubmit for register page works correctly", async () => {
+    render(
+      <MemoryRouter>
+        <RegisterForm onSubmit={mockProps} />
+      </MemoryRouter>
+    );
+    const nameInput = screen.getByPlaceholderText("Your name");
     const emailInput = screen.getByPlaceholderText("Your email");
     const passwordInput = screen.getByPlaceholderText("Your password");
-    const loginButton = screen.getByText("Login");
+    const registerButton = screen.getByText("Register");
+    expect(nameInput).toBeDefined();
     expect(emailInput).toBeDefined();
     expect(passwordInput).toBeDefined();
 
+    fireEvent.change(nameInput, { target: { value: "User" } });
     fireEvent.change(emailInput, { target: { value: "user@gmail.com" } });
     fireEvent.change(passwordInput, { target: { value: "User24@awesome" } });
-    fireEvent.click(loginButton);
+    fireEvent.click(registerButton);
 
     await waitFor(() => {
       expect(mockProps).toHaveBeenCalledTimes(1);
       expect(mockProps).toHaveBeenCalledWith({
+        name: "User",
         email: "user@gmail.com",
         password: "User24@awesome",
       });

ea8c9c700bee2f5097460ba223063ba8253088c4
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-10-02 23:08:35 +0700

    feat: :sparkles: Add content on input form in account info page

--- react-app/src/components/AccountInfoForm/index.tsx
+++ react-app/src/components/AccountInfoForm/index.tsx
@@ -1,8 +1,39 @@
 import { useNavigate } from "react-router-dom";
 import { Button, Card, Input, Typography } from "antd";
+import { useState, useEffect, useCallback } from "react";
 
 const AccountInfoForm = () => {
   const navigate = useNavigate();
+
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+  });
+
+  const token = sessionStorage.getItem("token");
+
+  const getAccount = useCallback(async () => {
+    const fetching = await fetch(
+      `https://mock-api.arikmpt.com/api/user/profile`,
+      {
+        headers: {
+          method: "GET",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const response = await fetching.json();
+
+    setUserData({
+      name: response.data.name,
+      email: response.data.email,
+    });
+    console.log(response);
+  }, [token]);
+
+  useEffect(() => {
+    getAccount();
+  }, [getAccount]);
 
   return (
     <>
@@ -12,7 +43,7 @@ const AccountInfoForm = () => {
             <Typography.Paragraph style={{ textAlign: "left" }}>
               {"Your Name"}
             </Typography.Paragraph>
-            <Input name={"name"} disabled />
+            <Input name={"name"} value={userData.name} disabled />
           </div>
           <div>
             <Typography.Paragraph
@@ -20,7 +51,7 @@ const AccountInfoForm = () => {
             >
               {"Your Email"}
             </Typography.Paragraph>
-            <Input name={"email"} disabled />
+            <Input name={"email"} value={userData.email} disabled />
           </div>
           <Button
             type={"default"}
